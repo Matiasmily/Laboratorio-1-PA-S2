9e570c27e3234ec9e2429bef8e1d12c4d1d6a790
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-07-29 09:47:01 -0600

    Segundo programa tienda en línea con POO

--- PA Laboratorio 1/Program.cs
+++ PA Laboratorio 1/Program.cs
@@ -1,2 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<double> preciosProductos = new List<double>();
+        int opcion = 0;
+        bool continueOne = true;
+        do
+        {
+            try
+            {
+                MostrarMenu();
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        IngresarProductos(preciosProductos);
+                        break;
+                    case 2:
+                        CalcularTotal(preciosProductos);
+                        break;
+                    case 3:
+                        Console.WriteLine("\nUsted está saliendo del programa...");
+                        Console.WriteLine("Presione Enter para continuar");
+                        Console.ReadKey();
+                        continueOne = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error: Por favor ingrese una opción numérica");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+            }
+        } while (continueOne);
+    }
+
+    static void MostrarMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("---- TIENDA EN LÍNEA ---");
+        Console.WriteLine("1. Ingresar Productos");
+        Console.WriteLine("2. Total de la compra");
+        Console.WriteLine("3. Salir");
+        Console.Write("Ingrese una opción: ");
+    }
+
+    static void IngresarProductos(List<double> preciosProductos)
+    {
+        Console.Clear();
+        Console.Write("Ingrese el nombre del producto: ");
+        string nombre = Console.ReadLine();
+        Console.Write("Ingrese el precio del producto: ");
+        double precio = double.Parse(Console.ReadLine());
+        preciosProductos.Add(precio);
+        Console.WriteLine($"Ha ingresado {preciosProductos.Count} productos");
+        Console.WriteLine("Presione enter para continuar...");
+        Console.ReadKey();
+    }
+
+    static double CalcularTotal(List<double> preciosProductos)
+    {
+        double total = 0;
+        foreach (double precio in preciosProductos)
+        {
+            total += precio;
+        }
+        if (total >= 200)
+        {
+            double descuento = total * 0.10;
+            total -= descuento;
+            Console.WriteLine($"El total de la compra es {total}, por la compra mayor a Q200.00 se le ha aplicado un descuento del 10%");
+        }
+        else
+        {
+            Console.WriteLine($"El total de la compra es: {total}");
+        }
+        Console.WriteLine("Presione enter para continuar...");
+        Console.ReadKey();
+        return total;
+    }
+}
---
+++ Tienda en linea POO/Productos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda_en_linea_POO
+{
+    internal class Productos
+    {
+    }
+}

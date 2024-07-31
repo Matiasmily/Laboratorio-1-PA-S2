7a1fcf8af387d95a0de5407d22d103baa7dd28eb
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-07-31 02:33:06 -0600

    Tienda en línea con POO

--- Tienda en linea POO/Productos.cs
+++ Tienda en linea POO/Productos.cs
@@ -6,7 +6,165 @@ using System.Threading.Tasks;
 
 namespace Tienda_en_linea_POO
 {
-    internal class Productos
+    public class Productos
     {
-    }
+        public string ID { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Productos(string iD, string nombre, double precio, int stock)
+        {
+            ID = iD;
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
+        }
+        public Productos(List<Productos> listaProductos) { }
+        public void MostrarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("--- MENÚ PRINCIPAL ---");
+            Console.WriteLine("1. Ingresar nuevo producto");
+            Console.WriteLine("2. Consultar información del producto");
+            Console.WriteLine("3. Venta de producto");
+            Console.WriteLine("4. Reabastecer stock");
+            Console.WriteLine("5. Actualizar precio");
+            Console.WriteLine("6. Salir");
+            Console.Write("Ingrese una opción: ");
+        }
+        public void IngresarProductos(List<Productos> listaProductos)
+        {
+            Console.Clear();
+            Console.WriteLine("--- INGRESAR PRODUCTOS ---");
+            Console.Write("ID: ");
+            string id = Console.ReadLine() ?? "";
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine()??"";
+            Console.Write("Precio: ");
+            double precio = double.Parse(Console.ReadLine()??"");
+            Console.Write("Cantidad: ");
+            int stock = int.Parse(Console.ReadLine()??"");
+            listaProductos.Add(new Productos(id, nombre, precio, stock));
+            Console.WriteLine("\nEl producto se ha añadido correctamente");
+            Console.WriteLine("\nPresione Enter para continuar");
+            Console.ReadKey();
+        }
+        public void ConsultarInformacion(List<Productos> listaProductos)
+        {
+            Console.Clear();
+            Console.WriteLine("--- CONSULTAR INFORMACIÓN ---");
+            Console.Write("ID: ");
+            string iD = Console.ReadLine() ?? "";
+            Productos? productoBuscar = listaProductos.Find(x=>x.ID == iD);
+            if (productoBuscar != null)
+            {
+                Console.WriteLine("Producto Encontrado");
+                Console.WriteLine($"\nID: {productoBuscar.ID}\nNombre: {productoBuscar.Nombre}\nPrecio: {productoBuscar.Precio}\nStock: {productoBuscar.Stock}");
+                Console.WriteLine("\nPresione Enter para continuar");
+                Console.ReadKey();
+            }else
+            {
+                Console.WriteLine("El producto no ha sido encontrado");
+                Console.WriteLine("\nPresione Enter para continuar");
+                Console.ReadKey();
+            }
+        }
+        public void VentaProducto(List<Productos> listaProductos)
+        {
+            Console.Clear();
+            Console.WriteLine("---  VENTA DE PRODUCTOS ---");
+            Console.Write("ID: ");
+            string iD = Console.ReadLine() ?? "";
+            Productos? productoBuscar = listaProductos.Find(x => x.ID == iD);
+            if (productoBuscar != null)
+            {
+                Console.Write("Cantidad: ");
+                int stock = int.Parse(Console.ReadLine()??"");
+                if (productoBuscar.Stock > 0)
+                {
+                    productoBuscar.Stock -= stock;
+                    Console.WriteLine("\nVenta realizada con éxito");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Producto sin existencia");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey(); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("El producto no ha sido encontrado");
+                Console.WriteLine("\nPresione Enter para continuar");
+                Console.ReadKey();
+            }
+        }
+        public void ReabastecerProductos(List<Productos> listaProductos)
+        {
+            Console.Clear();
+            Console.WriteLine("---  REABASTECER PRODUCTOS ---");
+            Console.Write("ID: ");
+            string iD = Console.ReadLine() ?? "";
+            Productos? productoBuscar = listaProductos.Find(x => x.ID == iD);
+            if (productoBuscar != null)
+            {
+                Console.Write("Cantidad: ");
+                int stock = int.Parse(Console.ReadLine() ?? "");
+                if (stock > 0)
+                {
+                    productoBuscar.Stock += stock;
+                    Console.WriteLine("\nProducto reabastecido con éxito");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un valor mayor a 0");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("El producto no ha sido encontrado");
+                Console.WriteLine("\nPresione Enter para continuar");
+                Console.ReadKey();
+            }
+        }
+        public void ActualizarPrecio(List<Productos> listaProductos)
+        {
+            Console.Clear();
+            Console.WriteLine("---  ACTUALIZAR PRECIO  ---");
+            Console.Write("ID: ");
+            string iD = Console.ReadLine() ?? "";
+            Productos? productoBuscar = listaProductos.Find(x => x.ID == iD);
+            if (productoBuscar != null)
+            {
+                Console.Write("Precio: ");
+                double precio = double.Parse(Console.ReadLine() ??"");
+                if (precio > 0)
+                {
+                    productoBuscar.Precio = precio;
+                    Console.WriteLine("\nPrecio del producto actualizado con éxito");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un valor mayor a cero");
+                    Console.WriteLine("\nPresione Enter para continuar");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("El producto no ha sido encontrado");
+                Console.WriteLine("\nPresione Enter para continuar");
+                Console.ReadKey();
+            }
+        }
+    } 
 }
--- Tienda en linea POO/Program.cs
+++ Tienda en linea POO/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Tienda_en_linea_POO;
+
+List <Productos > listaProductos = new List <Productos>();
+Productos producto = new Productos(listaProductos);
+ bool menu = true;
+
+do
+{
+    try
+    {
+        producto.MostrarMenu();
+        int opcion = int.Parse(Console.ReadLine());
+        switch (opcion)
+        {
+            case 1:
+                producto.IngresarProductos(listaProductos);
+                break;
+            case 2:
+                producto.ConsultarInformacion(listaProductos);
+                break;
+            case 3:
+                producto.VentaProducto(listaProductos);
+                break;
+            case 4:
+                producto.ReabastecerProductos(listaProductos);
+                break;
+            case 5:
+                producto.ActualizarPrecio(listaProductos);
+                break;
+            case 6:
+                Console.WriteLine("\nUsted está saliendo del programa...");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                menu = false;
+                break;
+            default:
+                Console.WriteLine("\nIngrese una opción válida");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+
+        }
+
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Error: Por favor ingrese una opción numérica");
+        Console.WriteLine("Presione Enter para continuar");
+        Console.ReadKey();
+    }
+} while (menu);
